6845348dc50d5343656185d7fb86240db1653e2f
shakilkhan12 <shakilkhan621@gmail.com>
2024-02-18 12:24:54 +0500

    first commit

---
+++ data/header.js
@@ -0,0 +1,9 @@
+const data = {
+  image: "/images/profile.jpg",
+  name: "rehan ullah khan",
+  jobTitle: "full stack javaScript & Blockchain developer",
+  address: "malakand, pakistan",
+  experience: "Almost 7 years of experience",
+  education: "BS Software engineering",
+};
+export default data;
